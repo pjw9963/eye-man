69522ab1e261dcf3987d7afc192efc6052f627bf
Patrick Wilbur <pjw9963@rit.edu>
2023-05-08 20:11:33 -0400

    initial commit

---
+++ Scripts/Diode.cs
@@ -0,0 +1,17 @@
+using Godot;
+using System;
+
+public partial class Diode : Node2D
+{
+
+    public override void _Ready()
+    {
+        base._Ready();
+
+    }
+
+    public override void _Process(double delta)
+    {
+
+    }
+}
---
+++ Scripts/Player.cs
@@ -0,0 +1,45 @@
+using Godot;
+using System;
+
+public partial class Player : CharacterBody2D
+{
+    [Export]
+    public int Speed { get; set; } = 400;
+    [Signal]
+    public delegate void ShootEventHandler(PackedScene laser, float direction, Vector2 location);
+    private PackedScene _laser = GD.Load<PackedScene>("res://Scenes/Laser.tscn");
+    private Node2D _diodeNode;
+
+    public override void _Ready()
+    {
+        base._Ready();
+
+        _diodeNode = GetNode<Node2D>("Diode");
+    }
+
+    public void GetInput()
+    {
+
+        Vector2 inputDirection = Input.GetVector("Left", "Right", "Up", "Down");
+        Velocity = inputDirection * Speed;
+        if (Input.IsActionJustPressed("Left") ||
+            Input.IsActionJustPressed("Right") ||
+            Input.IsActionJustPressed("Up") ||
+            Input.IsActionJustPressed("Down"))
+            Rotation = inputDirection.Angle();
+    }
+
+    public override void _PhysicsProcess(double delta)
+    {
+        GetInput();
+        MoveAndSlide();
+    }
+
+    public override void _Process(double delta)
+    {
+        if (Input.IsActionPressed("Fire"))
+        {
+            EmitSignal(SignalName.Shoot, _laser, Rotation, Position + _diodeNode.Position);
+        }
+    }
+}
